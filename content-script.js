c53944732efd709ba623b785fa48ccb84fc4e302
Kemal Ersin <mail@kemalersin.com>
2023-11-21 23:55:07 +0300

    background.js fixed.

--- content-script.js
+++ content-script.js
@@ -1,41 +1,36 @@
-const observer = new MutationObserver((mutationList) =>
-  mutationList.forEach((m) => {
-    chrome.storage.local.get("usdRate").then((result) => {
-      if (!result.usdRate) {
+chrome.storage.local.get("usdRate").then((result) => {
+  if (!result.usdRate) {
+    return;
+  }
+
+  const observer = new MutationObserver((m) => {
+    var regex = /\$([0-9,]+\.\d{2})\s*(USD)?/;
+    var elements = document.querySelectorAll(
+      ".price, .discount_final_price, .game_purchase_price, .game_area_dlc_price, .your_price div, " +
+        ".salepreviewwidgets_StoreSalePriceBox_Wh0L8"
+    );
+
+    elements.forEach((element) => {
+      if (element.matches(".u2t")) {
         return;
       }
 
-      var regex = /\$([0-9,]+\.\d{2})\s*(USD)?/;
-      var elements = document.querySelectorAll(
-        ".price, .discount_final_price, .game_purchase_price, .game_area_dlc_price, .your_price div, " +
-          ".salepreviewwidgets_StoreSalePriceBox_Wh0L8"
-      );
+      var match = element.textContent.match(regex);
 
-      elements.forEach((element) => {
-        if (element.matches(".u2t")) {
-          return;
-        }
+      if (match) {
+        var usdPrice = parseFloat(match[1]);
+        var multipliedPrice = usdPrice * result.usdRate;
 
-        var match = element.textContent.match(regex);
+        var formattedMultipliedPrice = multipliedPrice.toLocaleString("tr-TR", {
+          style: "currency",
+          currency: "TRY",
+        });
 
-        if (match) {
-          var usdPrice = parseFloat(match[1]);
-          var multipliedPrice = usdPrice * result.usdRate;
-
-          var formattedMultipliedPrice = multipliedPrice.toLocaleString(
-            "tr-TR",
-            {
-              style: "currency",
-              currency: "TRY",
-            }
-          );
-
-          element.classList.add("u2t");
-          element.textContent += ` (${formattedMultipliedPrice})`;
-        }
-      });
+        element.classList.add("u2t");
+        element.textContent += ` (${formattedMultipliedPrice})`;
+      }
     });
-  })
-);
+  });
 
-observer.observe(document, { childList: true, subtree: true });
+  observer.observe(document, { childList: true, subtree: true });
+});
