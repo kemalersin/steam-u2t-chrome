6536f5ac706662ddf0e6eec856e1238f3533f72e
Kemal Ersin <mail@kemalersin.com>
2023-11-21 23:45:33 +0300

    background.js simplified.

--- background.js
+++ background.js
@@ -1,42 +1,38 @@
-"use strict";
+const updateFrequency = 10;
+const apiUrl = "https://api.genelpara.com/embed/doviz.json";
 
-(function () {
-  const updateFrequency = 10;
-  const apiUrl = "https://api.genelpara.com/embed/doviz.json";
+function setAlarm() {
+  chrome.storage.local.get("updateFrequency", (result) => {
+    chrome.alarms.clear("update");
 
-  function setAlarm() {
-    chrome.storage.local.get("updateFrequency", (result) => {
-      chrome.alarms.clear("update");
-      
-      chrome.alarms.create("update", {
-        periodInMinutes: result.updateFrequency || updateFrequency,
-      });
+    chrome.alarms.create("update", {
+      periodInMinutes: result.updateFrequency || updateFrequency,
     });
-  }
+  });
+}
 
-  function getExchangeRate() {
-    fetch(apiUrl, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((response) =>
-        chrome.storage.local.set({ usdRate: response.USD.satis })
-      );
-  }
+function getExchangeRate() {
+  fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((response) =>
+      chrome.storage.local.set({ usdRate: response.USD.satis })
+    );
+}
 
-  chrome.runtime.onInstalled.addListener((details) => {
-    chrome.alarms.onAlarm.addListener((alarm) => getExchangeRate());
+chrome.runtime.onInstalled.addListener((details) => {
+  chrome.alarms.onAlarm.addListener((alarm) => getExchangeRate());
 
+  setAlarm();
+  getExchangeRate();
+});
+
+chrome.runtime.onMessage.addListener((msg, sender) => {
+  if (msg.updateAlarm) {
     setAlarm();
-    getExchangeRate();
-  });
-
-  chrome.runtime.onMessage.addListener((msg, sender) => {
-    if (msg.updateAlarm) {
-      setAlarm();
-    }
-  });
-})();
+  }
+});
