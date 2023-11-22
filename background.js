41a5e310615dd9e776a6522eaf7244b7854b2acb
Kemal Ersin <mail@kemalersin.com>
2023-11-22 13:06:12 +0300

    Background.js simplified.

--- background.js
+++ background.js
@@ -1,26 +1,22 @@
 const updateFrequency = 10;
 const apiUrl = "https://api.genelpara.com/embed/doviz.json";
 
-function getExchangeRate() {
-  fetch(apiUrl, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((response) =>
-      chrome.storage.local.set({ usdRate: response.USD.satis })
-    );
-}
-
 chrome.runtime.onInstalled.addListener(() => {
   chrome.alarms.create("update", {
+    delayInMinutes: 0,
     periodInMinutes: updateFrequency,
   });
 
-  chrome.alarms.onAlarm.addListener(() => getExchangeRate());
-
-  getExchangeRate();
+  chrome.alarms.onAlarm.addListener(() =>
+    fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) =>
+        chrome.storage.local.set({ usdRate: response.USD.satis })
+      )
+  );
 });
-
